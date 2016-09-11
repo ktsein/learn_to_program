ef62539f5a5b702ffc2b4ee22def7b1f36985951
Kyaw Tun Sein <kyawtunsein.gla@gmail.com>
2016-09-11 15:06:41 +0000

    Add ninety_nine_bottles_of_beer.rb Chap 10

--- ch10-nothing-new/ninety_nine_bottles_of_beer.rb
+++ ch10-nothing-new/ninety_nine_bottles_of_beer.rb
@@ -1,1 +1,14 @@
-# your code here
+num_at_start = 9999
+
+num_now = num_at_start
+
+while num_now > 2
+    puts num_now.to_s + ' bottles of beer on the wall, ' + num_now.to_s + 'bottles of beer!'
+    num_now = num_now - 1
+    puts 'Take one down, pass it around, ' + num_now.to_s + 'bottles of beer on the wall!'
+end
+
+puts "2 bottles of beer on the wall, 2 bottles of beer!"
+puts "Take one down, pass it around, 1 bottle of beer on the wall!"
+puts "1 bottle of beer on the wall, 1 bottle of beer!"
+puts "Take one down, pass it around, no more bottles of beer on the wall! =("
