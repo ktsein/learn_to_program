ad3da01f96bdb864a12a4dd1699c8ad156b79ba7
Kyaw Tun Sein <kyawtunsein.gla@gmail.com>
2016-09-12 05:05:18 +0000

    Add Happy_birthday.rb but error. SPANK! never stops =(

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,16 @@
-# your code here
+puts "What year were you born?"
+b_year = gets.chomp.to_i
+puts "What month were you born?"
+b_month = gets.chomp.to_i
+puts "What day were you born?"
+b_day = gets.chomp.to_i
+
+b = Time.local(b_year, b_month, b_day)
+t = Time.new
+
+age = 1
+
+while b <= t
+puts 'SPANK!'
+age = age + 1
+end
