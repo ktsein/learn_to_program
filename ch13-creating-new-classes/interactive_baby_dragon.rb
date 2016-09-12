d967edaabd3404196d2dbb7f5cf174226bf8b2a8
Kyaw Tun Sein <kyawtunsein.gla@gmail.com>
2016-09-12 08:46:17 +0000

    Add interactive_baby_dragon.rb Chap 13

--- ch13-creating-new-classes/interactive_baby_dragon.rb
+++ ch13-creating-new-classes/interactive_baby_dragon.rb
@@ -1,1 +1,27 @@
-# your code here
+
+puts 'What would you like to name your baby dragon?'
+name = gets.chomp
+pet = Dragon.new name
+
+
+while true
+puts 
+puts ' commands: feed, toss, walk, rock, put to bed, exit'
+command = gets.chomp
+
+if command == 'feed'
+    pet.feed
+elsif command == 'toss'
+    pet.toss
+elsif command == 'walk'
+    pet.walk
+elsif command == 'rock'
+    pet.rock 
+elsif command == 'put to bed'
+    pet.put_to_bed
+elseif command == 'exit'
+    exit
+else
+    puts 'Please only type the commands given.'
+end
+end
